0ea9dc269752464bf1a932fbf0c48e98c5851bf8
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2015-02-15 20:06:20 +0530

    Added requirements, Configuring multiple settings

---
+++ simpleme/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *
+
+SECRET_KEY = "localtestkey"
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+INSTALLED_APPS = (
+	'django_extension',
+	)
---
+++ simpleme/settings/production.py
@@ -0,0 +1,5 @@
+from .base import *
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+ALLOWED_HOSTS = ['*']
